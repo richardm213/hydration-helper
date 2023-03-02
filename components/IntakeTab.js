de7edefb07d46ef9fe87a5abdb14385c27266612
natalifisher <nfisher2@uci.edu>
2023-03-01 19:16:56 -0800

    Add notification upon recording water intake

--- components/IntakeTab.js
+++ components/IntakeTab.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {StyleSheet, Text, View, Alert} from 'react-native';
 import {Button, Input, Icon} from 'react-native-elements';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Notifications from 'expo-notifications';
 import DrinkSlider from './DrinkSlider';
 
 const white = '#fff';
@@ -40,6 +41,19 @@ const styles = StyleSheet.create({
   },
 });
 
+/* TODO: Modify to include percent water intake achieved
+and only be sent when some amount was recorded etc */
+async function intakeRecordNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Well done! Every intake record counts.',
+      body: 'Keep up the good work.',
+      data: {data: 'goes here'},
+    },
+    trigger: {seconds: 5},
+  });
+}
+
 export default function IntakeTab({intake, setIntake, unit}) {
   const [drinkAmount, setDrinkAmount] = useState(0);
   const [drinkType, setDrinkType] = useState('water');
@@ -76,6 +90,7 @@ export default function IntakeTab({intake, setIntake, unit}) {
           const newIntake = intake + drinkAmount;
           await AsyncStorage.setItem('@intake', newIntake.toString());
           setIntake(prev => prev + drinkAmount);
+          await intakeRecordNotification();
         }}
       />
     </View>
