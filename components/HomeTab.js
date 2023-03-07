c565d25f07a18aebe89b76205b6e8175b10e835c
Richard Meng <92076990+richardm213@users.noreply.github.com>
2023-03-07 15:16:06 -0800

    Add view details modal to Home

--- components/HomeTab.js
+++ components/HomeTab.js
@@ -1,15 +1,36 @@
 import {StyleSheet, View} from 'react-native';
-import {Icon, Text} from '@rneui/base';
+import {Button, Card, Icon, Text} from '@rneui/base';
+import Modal from 'react-native-modal';
+import {useState} from 'react';
 import COLORS from './Colors';
 
 const styles = StyleSheet.create({
+  cardStyle: {flexDirection: 'row', justifyContent: 'space-between'},
   container: {
     alignItems: 'center',
     backgroundColor: COLORS.white,
     flex: 1,
     justifyContent: 'center',
   },
-
+  largerTextWhite: {
+    alignItems: 'center',
+    color: COLORS.white,
+    fontSize: 22,
+    fontWeight: 'bold',
+    padding: 15,
+  },
+  modalContainer: {
+    backgroundColor: COLORS.white,
+    flex: 1,
+    marginHorizontal: 5,
+    marginVertical: 100,
+  },
+  submitButton: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 15,
+    height: 50,
+    marginTop: 25,
+  },
   suggestionIntakeText: {
     color: COLORS.primary,
     fontSize: 30,
@@ -38,9 +59,35 @@ const styles = StyleSheet.create({
 });
 
 export default function HomeTab({recommendation, unit, temperature}) {
+  const [isVisible, setIsVisible] = useState(false);
   const measurementType = unit === 'us-system' ? 'oz' : 'ml';
   return (
     <View style={styles.container}>
+      <Modal isVisible={isVisible}>
+        <View style={styles.modalContainer}>
+          <Button title="close" onPress={() => setIsVisible(false)} />
+          <Card wrapperStyle={styles.cardStyle}>
+            <Text>water</Text>
+            <Text>10 oz</Text>
+            <Text>10 am</Text>
+          </Card>
+          <Card wrapperStyle={styles.cardStyle}>
+            <Text>tea</Text>
+            <Text>14 oz</Text>
+            <Text>12 pm</Text>
+          </Card>
+          <Card wrapperStyle={styles.cardStyle}>
+            <Text>soda</Text>
+            <Text>23 oz</Text>
+            <Text>5 pm</Text>
+          </Card>
+          <Card wrapperStyle={styles.cardStyle}>
+            <Text>coffee</Text>
+            <Text>12 oz</Text>
+            <Text>8 pm</Text>
+          </Card>
+        </View>
+      </Modal>
       <View style={styles.temperatureView}>
         <Text h4>Weather: {temperature.toFixed(1)}&#8457;</Text>
       </View>
@@ -54,6 +101,12 @@ export default function HomeTab({recommendation, unit, temperature}) {
         <Text style={styles.suggestionIntakeText}>
           {recommendation.toFixed(0)} {measurementType}
         </Text>
+        <Button
+          title="View Details"
+          buttonStyle={styles.submitButton}
+          titleStyle={styles.largerTextWhite}
+          onPress={() => setIsVisible(true)}
+        />
       </View>
     </View>
   );
