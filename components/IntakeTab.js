26507963a5a85ea436e727ab64b02a85902d813f
natalifisher <nfisher2@uci.edu>
2023-03-08 01:10:20 -0800

    Update IntakeTab to add dropdown select list for drink types

--- components/IntakeTab.js
+++ components/IntakeTab.js
@@ -1,32 +1,30 @@
 import {useState} from 'react';
-import {StyleSheet, Text, View, Alert} from 'react-native';
-import {Button, Input, Icon} from '@rneui/base';
+import {StyleSheet, Text, Alert, ScrollView, SafeAreaView} from 'react-native';
+import {Button, Icon} from '@rneui/base';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
+import {SelectList} from 'react-native-dropdown-select-list';
 import DrinkSlider from './DrinkSlider';
 import COLORS from './Colors';
 import DRINKS from './Drinks';
 import DrinkEntry from './DrinkEntry';
 
 const styles = StyleSheet.create({
+  boxStyles: {marginHorizontal: 50, marginTop: 25},
   container: {
-    alignItems: 'center',
     backgroundColor: COLORS.white,
     flex: 1,
   },
+  dropDownStyles: {alignSelf: 'center', width: 200},
   icon: {
     marginTop: 30,
   },
-  input: {
-    marginLeft: 70,
-    marginRight: 70,
-  },
   largerTextBlue: {
-    alignItems: 'center',
     color: COLORS.primary,
     fontSize: 24,
     fontWeight: 'bold',
     margin: 20,
+    textAlign: 'center',
   },
   largerTextWhite: {
     alignItems: 'center',
@@ -35,10 +33,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     padding: 20,
   },
+  scrollView: {
+    marginBottom: 60,
+  },
   submitButton: {
     backgroundColor: COLORS.primary,
     borderRadius: 15,
     height: 50,
+    marginBottom: 60,
+    marginHorizontal: 125,
   },
 });
 
@@ -56,6 +59,14 @@ async function intakeRecordNotification(intake, recommendation) {
     trigger: {seconds: 5},
   });
 }
+
+const drinks = [
+  {key: '1', value: 'Water'},
+  {key: '2', value: 'Soda'},
+  {key: '3', value: 'Milk'},
+  {key: '4', value: 'Orange Juice'},
+  {key: '5', value: 'Coffee'},
+];
 
 export default function IntakeTab({intake, setIntake, recommendation, unit}) {
   const [drinkAmount, setDrinkAmount] = useState(0);
@@ -97,34 +108,39 @@ export default function IntakeTab({intake, setIntake, recommendation, unit}) {
   };
 
   return (
-    <View style={styles.container}>
-      <Icon
-        style={styles.icon}
-        name="local-drink"
-        type="material"
-        color={COLORS.primary}
-        size={160}
-      />
-      <Text style={styles.largerTextBlue}>
-        Please enter your new liquid intake below
-      </Text>
-      <DrinkSlider
-        drinkAmount={drinkAmount}
-        setDrinkAmount={setDrinkAmount}
-        unit={unit}
-      />
-      <Input
-        inputContainerStyle={styles.input}
-        placeholder="Drink type (ie. water)"
-        onChangeText={updateDrinkType}
-      />
-      <Button
-        disabled={submitDisabled}
-        buttonStyle={styles.submitButton}
-        titleStyle={styles.largerTextWhite}
-        title="Submit"
-        onPress={submitIntakeEntry}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Icon
+          style={styles.icon}
+          name="local-drink"
+          type="material"
+          color={COLORS.primary}
+          size={160}
+        />
+        <Text style={styles.largerTextBlue}>
+          Please enter your new liquid intake below
+        </Text>
+        <SelectList
+          setSelected={val => updateDrinkType(val)}
+          data={drinks}
+          save="value"
+          boxStyles={styles.boxStyles}
+          dropdownStyles={styles.dropDownStyles}
+          placeholder="Enter or select drink type"
+        />
+        <DrinkSlider
+          drinkAmount={drinkAmount}
+          setDrinkAmount={setDrinkAmount}
+          unit={unit}
+        />
+        <Button
+          disabled={submitDisabled}
+          buttonStyle={styles.submitButton}
+          titleStyle={styles.largerTextWhite}
+          title="Submit"
+          onPress={submitIntakeEntry}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
