3317829ea98b504181af73dddb9591fb0d667835
natalifisher <nfisher2@uci.edu>
2023-03-12 14:17:18 -0700

    Add list of weights in separate file

--- components/InputValues.js
+++ components/InputValues.js
@@ -8,8 +8,14 @@ for (let i = 36; i <= 96; i += 1) {
   heightList.push(`${i}`);
 }
 
+const weightList = [];
+for (let i = 50; i <= 350; i += 1) {
+  weightList.push(`${i}`);
+}
+
 const AGES = {
   ages: ageList,
   heights: heightList,
+  weights: weightList,
 };
 export default AGES;
