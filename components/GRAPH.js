4cfa5fe7e974e03c4aac4e51f24d0fee8d02f887
natalifisher <nfisher2@uci.edu>
2023-03-18 23:15:49 -0700

    Create separate list of shared graph props

---
+++ components/GRAPH.js
@@ -0,0 +1,11 @@
+const GRAPH = {
+  height: 380,
+  initialSpacing: 25,
+  labelWidth: 85,
+  noOfSections: 7,
+  spacing: 27,
+  xAxisThickness: 1,
+  yAxisThickness: 1,
+};
+
+export default GRAPH;
